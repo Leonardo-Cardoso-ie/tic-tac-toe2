8bd8ed4df4bf5bcd746f53d462d24560d51bb9d2
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2022-09-18 02:53:29 +0000

    Create a print board function

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#tic tac toe
+
+"""
+tic tac toe board
+[
+    [-, -, -],
+    [-, -, -]'
+    [-, -, -]
+
+]
+
+user_input -> something 1-9
+if the user enter anything else: tell them to go again
+check if the user_input is already taken
+add it to the board
+check if user won: checking rows, columns, diagonals
+toggle between users upon sucessful moves
+
+"""
+
+board = [
+
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+    ["_", "_", "_"]
+
+]
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(slot, end = "")
+        print()    
